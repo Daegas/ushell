54a29f32dbfaca9e72e8aa859e72b36e8949789c
Daegas <darae.gs@gmail.com>
2018-05-11 00:48:28 -0500

    Add files via upload

---
+++ nshell2.cc
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <vector> 
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+
+using namespace std;
+
+bool is_space(char C){
+  if (C==' ')return true;
+  if (C=='\t')return true;
+  return false;
+}
+
+void type_prompt(){
+    cout<<"Daras's ushell$ ";
+    cout.flush();
+}
+
+int arg_length(char* str){
+	int i=0;
+    while (!is_space(str[i])){
+	    i++;
+    	if(str[i]==0)break;
+    }
+    return i;
+}
+
+int space_length(char* str){
+	int i=0;
+    while (is_space(str[i])){
+	    i++;
+    	if(str[i]==0)break;
+    }
+    return i;
+}
+
+int arg_count(char* str){
+    char* s=str;
+    int n=0;
+    
+    while (true){
+        s+=space_length(s);
+        if(s[n]==0)break; 
+        if(s[0]==0)break;  
+        n++;
+        s+=arg_length(s);       
+    }
+    return n;
+}
+
+
+char** read_command(){
+    string command;
+    getline(cin,command); 
+    char* str=&command[0];
+    
+    int n=arg_count(str);
+    char** args=new char*[n+1];
+    char* temp;
+    
+    unsigned i=0;
+    
+    while(true){
+        str+=space_length(str);
+        if(str[0]==0)break;
+
+        int len = arg_length(str);
+        temp=new char[len+1];
+
+        for(int j=0; j<len ; j++) temp[j]=str[j];
+        temp[len]=0;
+        
+        args[i]=temp;
+        i++;
+
+        str+=len;
+        if(str[0]==0)break;
+    
+        return args;
+
+    }
+    args[i]=NULL;
+    return args;
+}
+
+int main(){
+    string aux;
+    char** argv;
+    char* prog;
+    pid_t child;  
+        
+    while(true){
+        type_prompt();
+        argv=read_command();
+        prog=&argv[0][0];
+        aux=prog;
+
+        if(aux=="quit" || aux=="exit")break;
+
+        if(aux=="cd"){
+            chdir(argv[1]);
+            continue;
+        }
+
+        child=fork();
+        if(child==0){
+            execvp(prog,argv);
+        }
+        else{
+            waitpid(child,NULL,0);        
+        }
+
+        unsigned i=0;
+
+        while(argv[i]!=NULL){
+	    	delete[] argv[i];//libera cadenas
+	    	i++;
+        }
+        delete [] argv;//libera vector
+ 
+    }    
+    
+ 
+
+   
+   
+        
+   cout<<"Bye :)"<<endl;
+    return 0;
+}
